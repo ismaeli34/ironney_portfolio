8f2b875101fbd72e7cc052f98710a48c3dff4ced
ismaeli34 <ronneyismael@gmail.com>
2025-10-17 13:20:11 +0200

    url fixed

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/portfolio/',
+    base: '/',
     build: {
         sourcemap: true,
         // terserOptions:
@@ -16,7 +16,6 @@ export default defineConfig({
         vue(),
         sitemap({
             hostname: 'https://hetari.github.io/',
-            basePath: 'portfolio',
             changefreq: 'hourly', // default: 'daily'
             priority: 1,
         }),
